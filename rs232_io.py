309a52e23be9f7a67bd916be349f77006ba95a4a
Franco <francosceballos@gmail>
2020-02-23 20:40:43 -0300

    Se modifica scrip para manejo de Uart con pyhon 3.7

--- rs232_io.py
+++ rs232_io.py
@@ -1,39 +1,54 @@
-#! /usr/bin/python
+#! /usr/bin/python3.7
+
+#Link libreria manejo puerto serie python3
+#https://pyserial.readthedocs.io/en/latest/pyserial_api.html?highlight=encode
+#https://julioecheverri.wordpress.com/2018/02/26/comunicacion-serial-y-python/
 
 import time
 import serial
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-    port='/dev/ttyUSB1',
-    baudrate=9600,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    write_timeout=2,
+    timeout=5
 )
 
 ser.isOpen()
 
-print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+print ('Enter your commands below.\r\nInsert "exit" to leave the application.')
 
-input=1
+inputcmb="AT\r\n"
 while 1 :
+    
     # get keyboard input
-    input = raw_input(">> ")
-        # Python 3 users
-        # input = input(">> ")
-    if input == 'exit':
+    #inputcmb = raw_input(">> ")
+    
+    # Python 3 users
+    inputcmb = input(">> ")
+    
+    if inputcmb == 'exit':
         ser.close()
         exit()
     else:
         # send the character to the device
         # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        ser.write(input + '\r\n')
+        ser.write((inputcmb + "\r\n").encode('utf-8'))
         out = ''
         # let's wait one second before reading output (let's give device time to answer)
         time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
+
+        size = ser.inWaiting()
+        print("\nDatos para leer en espera " + str(size))
+
+        out = ser.read(size)
+
+        #while ser.inWaiting() > 0:
+        #    out += ser.read(1)
 
         if out != '':
-            print ">>" + out
+            print (">>" + str(out) + "\n")
